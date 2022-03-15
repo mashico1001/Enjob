258e329eda54c6bb22f888c06818b966c5a51979
mashico1001 <wabumpr@gmail.com>
2022-03-15 11:09:52 +0000

    [Add]タスクの編集画面、削除機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,12 +16,20 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.user_id = current_user.id
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
