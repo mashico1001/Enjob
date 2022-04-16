65390e9e31a4c679d63dcac7827708a33e2b31c7
mashico1001 <wabumpr@gmail.com>
2022-04-16 15:52:18 +0000

    [Edit]refileのkey入力

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '10778e2cd0ef4bba614c693c30a54dee1de1e7c8428804fc2e3417177c9c48081ffd8c83cac47edf5eedb0fabba700fdb73fb8b1622f6f6f364550383fcb5259'
